cb219c1996053017648046c113ecda421f140e2f
osamaKu <osamaklaib1981@gmail.com>
2020-05-21 01:16:29 +0300

    core types

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // Core Types
-function add(num1, num2) {
+function add(num1, num2, showResult) {
+    // // We do not need the below if with TS
+    // // type script catch this error in development mode, typescript (staticky type) javascript (dynamically type)
+    // if(typeof num1 !== 'number' || typeof num2 !== 'number') {
+    //   throw new Error('Incorrect Input!')
+    // }
     return num1 + num2;
 }
+// const number1 = '5'; // result = 53.6 read number as string
 var number1 = 5;
-var number2 = 3.6;
-var result = add(number1, number2);
+var number2 = 3.6; // TS have one number type (number) NO Float OR Decimal ...etc 1, 4.5. -10 have the same type(number)
+var printResult = true;
+var result = add(number1, number2, printResult);
 console.log(result);
--- app.ts
+++ app.ts
@@ -1,18 +1,28 @@
 // Core Types
-function add(num1: number, num2: number): number {
+function add(
+  num1: number,
+  num2: number,
+  showResult: boolean,
+): number | undefined {
   // // We do not need the below if with TS
   // // type script catch this error in development mode, typescript (staticky type) javascript (dynamically type)
   // if(typeof num1 !== 'number' || typeof num2 !== 'number') {
   //   throw new Error('Incorrect Input!')
   // }
 
-  return num1 + num2;
+  if (showResult) {
+    console.log(num1 + num2);
+  } else {
+    return num1 + num2;
+  }
 }
 
 // const number1 = '5'; // result = 53.6 read number as string
 const number1 = 5;
 const number2 = 3.6; // TS have one number type (number) NO Float OR Decimal ...etc 1, 4.5. -10 have the same type(number)
 
-const result = add(number1, number2);
+const printResult = true;
+
+const result = add(number1, number2, printResult);
 
 console.log(result);
