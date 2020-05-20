60f4da2d5d9d5e2e64ac88c0b5a99108614e8944
osamaKu <osamaklaib1981@gmail.com>
2020-05-21 01:53:51 +0300

    inital and defined variables with its type

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 // Core Types
-function add(num1, num2, showResult) {
+function add(num1, num2, showResult, phrase) {
     // // We do not need the below if with TS
     // // type script catch this error in development mode, typescript (staticky type) javascript (dynamically type)
     // if(typeof num1 !== 'number' || typeof num2 !== 'number') {
     //   throw new Error('Incorrect Input!')
     // }
-    return num1 + num2;
+    var result = num1 + num2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return num1 + num2;
+    }
 }
 // const number1 = '5'; // result = 53.6 read number as string
 var number1 = 5;
 var number2 = 3.6; // TS have one number type (number) NO Float OR Decimal ...etc 1, 4.5. -10 have the same type(number)
 var printResult = true;
-var result = add(number1, number2, printResult);
-console.log(result);
+var resultPhrase = "Result is ";
+// let sumNum: number = 5; // Bad brackets we dont add type to initial variable!
+// let sumNum: number; // is a good brackets we add type to defined variable!
+add(number1, number2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ function add(
   num1: number,
   num2: number,
   showResult: boolean,
+  phrase: string,
 ): number | undefined {
   // // We do not need the below if with TS
   // // type script catch this error in development mode, typescript (staticky type) javascript (dynamically type)
@@ -10,8 +11,9 @@ function add(
   //   throw new Error('Incorrect Input!')
   // }
 
+  const result = num1 + num2;
   if (showResult) {
-    console.log(num1 + num2);
+    console.log(phrase + result);
   } else {
     return num1 + num2;
   }
@@ -22,7 +24,9 @@ const number1 = 5;
 const number2 = 3.6; // TS have one number type (number) NO Float OR Decimal ...etc 1, 4.5. -10 have the same type(number)
 
 const printResult = true;
+const resultPhrase = "Result is ";
 
-const result = add(number1, number2, printResult);
+// let sumNum: number = 5; // Bad brackets we dont add type to initial variable!
+// let sumNum: number; // is a good brackets we add type to defined variable!
 
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
